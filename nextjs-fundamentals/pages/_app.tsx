b645ceffbc671e3c84a45f57d68fb4e75da070d8
Ulzahk <ulzahk@gmail.com>
2022-02-09 07:28:37 -0500

    [UPDATE] tsconfig to allows components/* path

--- nextjs-fundamentals/pages/_app.tsx
+++ nextjs-fundamentals/pages/_app.tsx
@@ -1,5 +1,5 @@
 import {  AppProps } from 'next/app'
-import Layout from '../components/Layout/Layout'
+import Layout from '@components/Layout/Layout'
 
 function MyApp({ Component, pageProps }: AppProps)  {
   // Providers - Contexts, Theme, Data
