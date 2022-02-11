5e10c04be433f7614fcc2db1c2c2b01317dfdbe4
Ulzahk <ulzahk@gmail.com>
2022-02-11 08:55:31 -0500

    [UPDATE] Add getStaticPaths to product dynamic file

--- nextjs-fundamentals/pages/product/[id].tsx
+++ nextjs-fundamentals/pages/product/[id].tsx
@@ -1,24 +1,37 @@
-import React, { useState, useEffect } from 'react'
-import { useRouter } from 'next/router'
+import React from 'react'
+import { GetStaticProps } from 'next'
 
 import Layout from '@components/Layout/Layout'
 import ProductSummary from '@components/ProductSummary/ProductSummary'
 
-const ProductPage = () => {
-  const { query } = useRouter()
-  const [product, setProduct] = useState<TProduct | null>(null)
+export const getStaticPaths = async () => {
+  const response  = await fetch('http://localhost:3000/api/avo')
+  const { data: productList }: TAPIAvoResponse = await response.json()
 
-  useEffect(() => {
-    if (query.id) {
-      window
-        .fetch(`/api/avo/${query.id}`)
-        .then((response) => response.json())
-        .then((data: TProduct) => {
-          setProduct(data)
-        })
+  const paths = productList.map(({ id }) => ({
+    params: {
+      id
     }
-  }, [query.id])
+  }))
 
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export const getStaticProps: GetStaticProps  = async ({ params }) => {
+  const id = params?.id as string
+  const response  = await fetch(`http://localhost:3000/api/avo/${id}`)
+  const product: TProduct = await response.json()
+  return {
+    props: {
+      product
+    }
+  }
+}
+
+const ProductPage = ({product}: {product: TProduct}) => {
   return (
     <Layout>
       {product == null ? null : <ProductSummary product={product} />}
